0b5b9481c63ffb2721e7b97c676627d1f9f2d167
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2016-12-18 14:40:42 +0800

    ditambahkan fungsi untuk mengganti ukuran thumbnail

--- js/recent-post-with-thumb.js
+++ js/recent-post-with-thumb.js
@@ -101,3 +101,15 @@ function labelthumbs(json) {
     }
     document.write('</ul>');
 }
+
+
+function changeThumbSize(id, size) {
+    var blogGadget = document.getElementById(id);
+    var replacement = blogGadget.innerHTML;
+    blogGadget.innerHTML = replacement.replace(/s72-c/g, 's'+ size + '-c');
+    var thumbnails = blogGadget.getElementsByTagName('img');
+    for (var i = 0; i & lt; thumbnails.length; i++) {
+        thumbnails[i].width = size;
+        thumbnails[i].height = size;
+    }
+}
